04a9da87cca03ce1a53d4d99edf20d7af19c7dc5
Erick-Fleury <erick_fleury@hotmail.com>
2023-04-06 00:04:39 -0300

    Acertando codigo JS

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-var nome = window.prompt( 'Olá, seja bem vindo! Qual seu nome?')
-window.alert (`É um prazer te receber,${nome}${!}` )
+var nome = window.prompt("Olá, seja bem vindo! Qual seu nome?");
+window.alert(
+  "Prazer em te receber," +
+    nome +
+    " !" +
+    " " +
+    "Insira nos campos abaixo suas tarefas."
+);
