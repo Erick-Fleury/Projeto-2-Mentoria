64d149d8b4fef2fada4f1cb8fa1bd24ab5ab2852
Erick-Fleury <erick_fleury@hotmail.com>
2023-04-07 19:53:22 -0300

    Ajustes esboco projeto

--- app.js
+++
@@ -1,8 +0,0 @@
-var nome = window.prompt("Olá, seja bem vindo! Qual seu nome?");
-window.alert(
-  "Prazer em te receber," +
-    nome +
-    " !" +
-    " " +
-    "Insira nos campos abaixo suas tarefas."
-);
