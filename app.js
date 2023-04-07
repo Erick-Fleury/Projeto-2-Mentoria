282064272cd0eb4a7bbd3e0d1d950cb5c2428710
Erick-Fleury <erick_fleury@hotmail.com>
2023-04-07 20:55:52 -0300

    Adicionando JS

---
+++ app.js
@@ -0,0 +1,11 @@
+// Seleção elementos
+const todoForm = document.querySelector("task-form");
+const todoInput = document.querySelector("input-tarefa");
+const todoList = document.querySelector("list-todo");
+const editForm = document.querySelector("edit-form");
+const editinput = document.querySelector("input-edit");
+const Cancelbtn = document.querySelector("btn-cancel-edit");
+
+// Funções
+
+// Eventos
